b3d44058c86c39cca07da7e1a1a01837b2026283
Dipen Chauhan <get.dipen@gmail.com>
2022-03-19 00:36:33 -0700

    Email to user

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,13 +1,7 @@
 class WelcomeMailer < ApplicationMailer
+  def send_greetings_notification(user)
+    @user = user
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.welcome_mailer.send_greetings_notification.subject
-  #
-  def send_greetings_notification
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    mail to: user.email, subject: "Thank you for signing up"
   end
 end
