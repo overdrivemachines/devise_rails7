0bbb7bb251ad85701fa05bf64ccede2880b8c0c0
Dipen Chauhan <get.dipen@gmail.com>
2022-03-20 21:22:53 -0700

    Login using username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,16 +30,35 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :trackable
+         :confirmable, :trackable,
+         authentication_keys: [:login]
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  # only allow letters and numbers
+  validates :username, format: { with: /\A[a-zA-Z0-9]+\Z/ }
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :contact_number, presence: true
   validates :address, presence: true
 
+  attr_accessor :login
+
+  def login
+    @login || self.username || self.email
+  end
+
   private
   def after_confirmation
     WelcomeMailer.send_greetings_notification(self).deliver_later
+  end
+
+  def self.find_for_database_authentication(warden_condition)
+    conditions = warden_condition.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(username) = :value OR
+        lower(email) = :value", { value: login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
   end
 end
