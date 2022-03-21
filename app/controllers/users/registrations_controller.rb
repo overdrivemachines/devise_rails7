45a83bd8542c9e39ec1536984d23d37ac05c8629
Dipen Chauhan <get.dipen@gmail.com>
2022-03-20 21:21:38 -0700

    Added email in configure_sign_up_params

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -44,7 +44,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up,
       keys: [:first_name, :last_name, :contact_number, :address,
-        :username, :password, :password_confirmation])
+        :username, :email, :password, :password_confirmation])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
